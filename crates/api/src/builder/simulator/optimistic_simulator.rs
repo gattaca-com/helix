bf458e64c4ea7cf3638902371e7f7a56a1969dc2
gd-0 <90608901+gd-0@users.noreply.github.com>
2024-01-24 20:58:35 +0000

    comment out demote_builder_due_to_error

--- crates/api/src/builder/simulator/optimistic_simulator.rs
+++ crates/api/src/builder/simulator/optimistic_simulator.rs
@@ -71,7 +71,7 @@ impl<A: Auctioneer + 'static, DB: DatabaseService + 'static> OptimisticSimulator
                         err=%err,
                         "Block simulation resulted in an error. Demoting builder...",
                     );
-                    self.demote_builder_due_to_error(&request.message.builder_public_key, request.execution_payload.block_hash(), err.to_string()).await;
+                    //self.demote_builder_due_to_error(&request.message.builder_public_key, request.execution_payload.block_hash(), err.to_string()).await;
                 }
             }
             return Err(err);
