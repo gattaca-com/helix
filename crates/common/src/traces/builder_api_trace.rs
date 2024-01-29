f5d4457ad4024f1facea64a936f6a9cbc1f5c6c7
gd-0 <90608901+gd-0@users.noreply.github.com>
2024-01-29 13:17:27 +0000

    refactor

--- crates/common/src/traces/builder_api_trace.rs
+++ crates/common/src/traces/builder_api_trace.rs
@@ -2,9 +2,9 @@
 pub struct SubmissionTrace {
     pub receive: u64,
     pub decode: u64,
+    pub floor_bid_checks: u64,
     pub pre_checks: u64,
     pub signature: u64,
-    pub floor_bid_checks: u64,
     pub simulation: u64,
     pub auctioneer_update: u64,
     pub request_finish: u64,
