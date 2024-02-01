3d58250be6a1b2242c779967e948af5babcdda12
gd-0 <90608901+gd-0@users.noreply.github.com>
2024-02-01 16:46:56 +0000

    fix housekeeper_tests

--- crates/housekeeper/src/housekeeper_tests.rs
+++ crates/housekeeper/src/housekeeper_tests.rs
@@ -11,13 +11,15 @@ mod housekeeper_tests {
 
     // ++++ IMPORTS ++++
     use crate::housekeeper::{
-        Housekeeper, HEAD_EVENT_CHANNEL_SIZE, SLEEP_DURATION_BEFORE_REFRESHING_VALIDATORS,
+        Housekeeper, SLEEP_DURATION_BEFORE_REFRESHING_VALIDATORS,
     };
-    use helix_beacon_client::mock_multi_beacon_client::MockMultiBeaconClient;
+    use helix_beacon_client::{mock_multi_beacon_client::MockMultiBeaconClient, MultiBeaconClientTrait};
     use helix_common::{api::builder_api::BuilderGetValidatorsResponseEntry, ValidatorSummary};
     use helix_database::MockDatabaseService;
     use helix_datastore::MockAuctioneer;
-    use tokio::task;
+    use tokio::{sync::broadcast, task};
+
+    const HEAD_EVENT_CHANNEL_SIZE: usize = 100;
 
     // ++++ HELPERS ++++
     fn get_housekeeper() -> HelperVars {
@@ -36,7 +38,7 @@ mod housekeeper_tests {
             proposer_duties_has_been_read.clone(),
         );
         let auctioneer = MockAuctioneer::new();
-        let housekeeper = Housekeeper::new(Arc::new(db), beacon_client, auctioneer);
+        let housekeeper = Housekeeper::new(Arc::new(db), beacon_client.clone(), auctioneer);
 
         HelperVars {
             housekeeper,
@@ -46,14 +48,18 @@ mod housekeeper_tests {
             proposer_duties,
             state_validators_has_been_read,
             proposer_duties_has_been_read,
+            beacon_client
         }
     }
 
-    fn start_housekeeper(
+    async fn start_housekeeper(
         housekeeper: Arc<Housekeeper<MockDatabaseService, MockMultiBeaconClient, MockAuctioneer>>,
+        beacon_client: MockMultiBeaconClient,
     ) {
+        let (head_event_sender, mut head_event_receiver) = broadcast::channel(HEAD_EVENT_CHANNEL_SIZE);
+        beacon_client.subscribe_to_head_events(head_event_sender).await;
         task::spawn(async move {
-            housekeeper.start().await.unwrap();
+            housekeeper.start(&mut head_event_receiver).await.unwrap();
         });
     }
 
@@ -66,46 +72,37 @@ mod housekeeper_tests {
         pub proposer_duties: Arc<Mutex<Vec<BuilderGetValidatorsResponseEntry>>>,
         pub state_validators_has_been_read: Arc<AtomicBool>,
         pub proposer_duties_has_been_read: Arc<AtomicBool>,
+        pub beacon_client: MockMultiBeaconClient,
     }
 
     // ++++ TESTS ++++
     #[tokio::test]
-    async fn test_beacon_client_subscribes_to_head_events() {
-        let vars = get_housekeeper();
-        start_housekeeper(vars.housekeeper.clone());
-
-        tokio::time::sleep(Duration::from_millis(100)).await;
-
-        assert!(vars.subscribed_to_head_events.load(std::sync::atomic::Ordering::Relaxed));
-    }
-
-    #[tokio::test]
     async fn test_head_event_is_processed_by_housekeeper() {
         let vars = get_housekeeper();
-        start_housekeeper(vars.housekeeper.clone());
+        start_housekeeper(vars.housekeeper.clone(), vars.beacon_client).await;
         tokio::time::sleep(Duration::from_millis(10)).await;
 
-        // assert that the capacity of the channel is correct at HEAD_EVENT_CHANNEL_SIZE - 1 as the
+        // assert that the len of the channel is correct at 1 as the
         // beacon client sends a dummy event
         assert!(
             vars.chan_head_events_capacity.load(std::sync::atomic::Ordering::Relaxed) ==
-                HEAD_EVENT_CHANNEL_SIZE - 1
+                1
         );
         tokio::time::sleep(Duration::from_millis(100)).await;
 
-        // assert that the capacity of the channel is correct at HEAD_EVENT_CHANNEL_SIZE as the
+        // assert that the len of the channel is correct at 0 as the
         // housekeeper has processed the dummy event
         assert!(vars.subscribed_to_head_events.load(std::sync::atomic::Ordering::Relaxed));
         assert!(
             vars.chan_head_events_capacity.load(std::sync::atomic::Ordering::Relaxed) ==
-                HEAD_EVENT_CHANNEL_SIZE
+                0
         );
     }
 
     #[tokio::test]
     async fn test_known_validators_are_set() {
         let vars = get_housekeeper();
-        start_housekeeper(vars.housekeeper.clone());
+        start_housekeeper(vars.housekeeper.clone(), vars.beacon_client).await;
         tokio::time::sleep(
             SLEEP_DURATION_BEFORE_REFRESHING_VALIDATORS + Duration::from_millis(100),
         )
@@ -120,7 +117,7 @@ mod housekeeper_tests {
     #[tokio::test]
     async fn test_proposer_duties_set() {
         let vars = get_housekeeper();
-        start_housekeeper(vars.housekeeper.clone());
+        start_housekeeper(vars.housekeeper.clone(), vars.beacon_client).await;
         tokio::time::sleep(Duration::from_millis(100)).await;
 
         assert!(vars.proposer_duties.lock().unwrap().len() == 1);
@@ -132,7 +129,7 @@ mod housekeeper_tests {
     #[tokio::test]
     async fn test_proposer_duties_have_been_read() {
         let vars = get_housekeeper();
-        start_housekeeper(vars.housekeeper.clone());
+        start_housekeeper(vars.housekeeper.clone(), vars.beacon_client).await;
         tokio::time::sleep(Duration::from_millis(100)).await;
 
         assert!(vars.proposer_duties_has_been_read.load(std::sync::atomic::Ordering::Relaxed));
@@ -141,7 +138,7 @@ mod housekeeper_tests {
     #[tokio::test]
     async fn test_state_validators_have_been_read() {
         let vars = get_housekeeper();
-        start_housekeeper(vars.housekeeper.clone());
+        start_housekeeper(vars.housekeeper.clone(), vars.beacon_client).await;
         tokio::time::sleep(
             SLEEP_DURATION_BEFORE_REFRESHING_VALIDATORS + Duration::from_millis(100),
         )
