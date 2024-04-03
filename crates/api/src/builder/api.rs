397082b80a3e2826bc071b47ac681c9e851c7953
owen <owen@gattaca.com>
2024-04-03 16:51:19 +0100

    bug fix: need to always read request

--- crates/api/src/builder/api.rs
+++ crates/api/src/builder/api.rs
@@ -190,17 +190,17 @@ where
             timestamp_request_start = trace.receive,
         );
 
+        // Decode the incoming request body into a payload
+        let (payload, is_cancellations_enabled) =
+            decode_payload(req, &mut trace, &request_id).await?;
+        let block_hash = payload.message().block_hash.clone();
+
         // Verify that we have a validator connected for this slot
         if next_duty.is_none() {
             warn!(request_id = %request_id, "could not find slot duty");
             return Err(BuilderApiError::ProposerDutyNotFound);
         }
         let next_duty = next_duty.unwrap();
-
-        // Decode the incoming request body into a payload
-        let (payload, is_cancellations_enabled) =
-            decode_payload(req, &mut trace, &request_id).await?;
-        let block_hash = payload.message().block_hash.clone();
 
         debug!(
             request_id = %request_id,
@@ -396,17 +396,17 @@ where
             timestamp_request_start = trace.receive,
         );
 
+        // Decode the incoming request body into a payload
+        let (mut payload, is_cancellations_enabled) =
+            decode_header_submission(req, &mut trace, &request_id).await?;
+        let block_hash = payload.block_hash().clone();
+
         // Verify that we have a validator connected for this slot
         if next_duty.is_none() {
             warn!(request_id = %request_id, "could not find slot duty");
             return Err(BuilderApiError::ProposerDutyNotFound);
         }
         let next_duty = next_duty.unwrap();
-
-        // Decode the incoming request body into a payload
-        let (mut payload, is_cancellations_enabled) =
-            decode_header_submission(req, &mut trace, &request_id).await?;
-        let block_hash = payload.block_hash().clone();
 
         debug!(
             request_id = %request_id,
