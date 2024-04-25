cb7fb59b163b731d6e3f57a43fbfab6f361ef7ee
gd-0 <90608901+gd-0@users.noreply.github.com>
2024-04-25 20:03:49 +0100

    add serde defaults to validator preferences

--- crates/common/src/validator_preferences/mod.rs
+++ crates/common/src/validator_preferences/mod.rs
@@ -2,10 +2,12 @@
 pub struct ValidatorPreferences {
     /// A boolean flag indicating whether the validator requests the relay
     /// to enforce censoring of sanctioned transactions.
+    #[serde(default = "default_censoring")]
     pub censoring: bool,
     /// An optional list of BuilderIDs. If this is set, the relay will only accept
     /// submissions from builders whose public keys are linked to the IDs in this list.
     /// This allows for limiting submissions to a trusted set of builders.
+    #[serde(default)]
     pub trusted_builders: Option<Vec<String>>,
 
     /// Allows validators to express a preference for whether a delay should be applied to get headers or not.
@@ -13,6 +15,9 @@ pub struct ValidatorPreferences {
     pub header_delay: bool,
 }
 
+fn default_censoring() -> bool {
+    false
+}
 fn default_header_delay() -> bool {
     true
 }
