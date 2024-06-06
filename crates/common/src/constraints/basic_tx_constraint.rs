9ea10fa646e1c8beee5d5f131a45d87889970083
gd-0 <90608901+gd-0@users.noreply.github.com>
2024-06-06 13:36:03 +0100

    verify constraints in builder api

--- crates/common/src/constraints/basic_tx_constraint.rs
+++ crates/common/src/constraints/basic_tx_constraint.rs
@@ -1,10 +1,20 @@
 use ethereum_consensus::bellatrix::mainnet::Transaction;
 use ethereum_consensus::primitives::Bytes32;
 use ethereum_consensus::ssz::prelude::*;
+use crate::constraints::Constraint;
 
 
 #[derive(Debug, Clone, SimpleSerialize, serde::Serialize, serde::Deserialize)]
 pub struct BasicTransactionConstraint {
     pub hash: Bytes32,
     pub transaction_bytes: Transaction,
+}
+
+impl BasicTransactionConstraint {
+    /// Verifies that the constraint is valid for an ordered list of hashes.
+    ///
+    /// Returns true if the constraint is valid, false if not.
+    pub fn verify_from_tx_hash_vec(&self, tx_hashes: &Vec<Bytes32>) -> bool {
+        tx_hashes.contains(&self.hash)
+    }
 }
