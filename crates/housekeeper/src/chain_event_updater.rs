a0e3b63af182b125227ca572ff8152ade8938693
thedevbirb <lorenzo.feroleto.dev@gmail.com>
2024-10-03 13:17:04 +0200

    chore: use chain_info.seconds_per_slot instead of 12 seconds constant

--- crates/housekeeper/src/chain_event_updater.rs
+++ crates/housekeeper/src/chain_event_updater.rs
@@ -5,7 +5,7 @@ use std::{
 use std::collections::HashMap;
 
 use ethereum_consensus::{
-    configs::{goerli::CAPELLA_FORK_EPOCH, mainnet::SECONDS_PER_SLOT},
+    configs::goerli::CAPELLA_FORK_EPOCH,
     primitives::Bytes32,
 };
 use tokio::sync::{broadcast, mpsc};
@@ -132,7 +132,7 @@ impl<D: DatabaseService> ChainEventUpdater<D> {
         // Validate this isn't a faulty head slot
         if let Ok(current_timestamp) = SystemTime::now().duration_since(UNIX_EPOCH) {
             let slot_timestamp =
-                self.chain_info.genesis_time_in_secs + (event.slot * SECONDS_PER_SLOT);
+                self.chain_info.genesis_time_in_secs + (event.slot * self.chain_info.seconds_per_slot);
             if slot_timestamp > current_timestamp.as_secs() + MAX_DISTANCE_FOR_FUTURE_SLOT {
                 warn!(head_slot = event.slot, "head event slot is too far in the future",);
                 return;
