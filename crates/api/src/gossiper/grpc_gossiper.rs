082a4950cfc15b5658b79f38c4b0d8bad3bdfa5d
eltitanb <lorenzo@gattaca.com>
2025-04-25 10:55:28 +0100

    add back comments

--- crates/api/src/gossiper/grpc_gossiper.rs
+++ crates/api/src/gossiper/grpc_gossiper.rs
@@ -261,6 +261,8 @@ impl GrpcGossiperClientManager {
 }
 
 impl GrpcGossiperClientManager {
+    /// Broadcast a header. The header will be saved if it is the best header for the receiving
+    /// relay. Only validated Headers are gossiped.
     pub async fn broadcast_header(&self, request: BroadcastHeaderParams) {
         let request = request.to_proto();
 
@@ -275,6 +277,9 @@ impl GrpcGossiperClientManager {
         }
     }
 
+    /// Broadcast a payload. This payload will always be saved to the receiving relay's Autcioneer.
+    /// This is because, the local relay has saved the payload's header and may have served it for
+    /// get_header. Only validated Payloads are gossiped.
     pub async fn broadcast_payload(&self, request: BroadcastPayloadParams) {
         let request = request.to_proto();
 
@@ -289,6 +294,9 @@ impl GrpcGossiperClientManager {
         }
     }
 
+    /// Broadcast a request for a payload. If the receiving relay has the payload, it will be able
+    /// to broadcast the block to the network. This is fallback mechanism for when the relay that
+    /// get_header was called on does not have the payload yet.
     pub async fn broadcast_get_payload(&self, request: BroadcastGetPayloadParams) {
         let request = request.to_proto();
 
