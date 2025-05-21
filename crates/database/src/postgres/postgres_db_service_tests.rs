3af26b2b3165841795b0a5d03779fbfc3907e0ac
Jay Rabjohns <jay.r@gattaca.com>
2025-05-21 13:00:04 +0100

    lint

--- crates/database/src/postgres/postgres_db_service_tests.rs
+++ crates/database/src/postgres/postgres_db_service_tests.rs
@@ -193,8 +193,8 @@ mod tests {
             let result = result
                 .iter()
                 .find(|r| {
-                    r.registration_info.registration.message.pubkey
-                        == registration.registration.message.pubkey
+                    r.registration_info.registration.message.pubkey ==
+                        registration.registration.message.pubkey
                 })
                 .unwrap();
             assert_eq!(
