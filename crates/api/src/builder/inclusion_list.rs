cd27727d935aedfb27875bd855f857233961ab63
Jay Rabjohns <52382074+jayrabjohns@users.noreply.github.com>
2025-06-02 11:09:50 +0100

    fix: add debug info for inclusion lists (#160)

--- crates/api/src/builder/inclusion_list.rs
+++ crates/api/src/builder/inclusion_list.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 use axum::{extract::Path, response::IntoResponse, Extension};
 use helix_common::utils::get_slot_coordinate;
 use hyper::StatusCode;
-use tracing::debug;
+use tracing::info;
 
 use super::{api::BuilderApi, error::BuilderApiError, InclusionListPathParams};
 use crate::Api;
@@ -14,29 +14,35 @@ impl<A: Api> BuilderApi<A> {
         Extension(api): Extension<Arc<BuilderApi<A>>>,
         Path(InclusionListPathParams { slot, parent_hash, pub_key }): Path<InclusionListPathParams>,
     ) -> Result<impl IntoResponse, BuilderApiError> {
-        debug!(requested_slot = %slot, pub_key = %pub_key, parent_hash = %parent_hash,
+        info!(requested_slot = %slot, pub_key = %pub_key, parent_hash = %parent_hash,
             "Request for inclusion list."
         );
 
         let current_list = api.current_inclusion_list.read();
 
         let Some(current_list) = current_list.as_ref() else {
-            debug!(requested_slot = %slot, pub_key = %pub_key, parent_hash = %parent_hash,
+            info!(requested_slot = %slot, pub_key = %pub_key, parent_hash = %parent_hash,
                 "Builder has requested an inclusion list but none has been found in redis."
             );
-            return Ok(StatusCode::NOT_FOUND.into_response());
+            return Ok(
+                (StatusCode::NOT_FOUND, "No inclusion lists have been generated").into_response()
+            );
         };
 
-        let current_slot_coordinate = get_slot_coordinate(slot as i32, &pub_key, &parent_hash);
+        let requested_slot_coordinate = get_slot_coordinate(slot as i32, &pub_key, &parent_hash);
 
-        if current_list.slot_coordinate == current_slot_coordinate {
+        if current_list.slot_coordinate == requested_slot_coordinate {
             Ok((StatusCode::OK, axum::Json(current_list.inclusion_list.clone())).into_response())
         } else {
-            debug!(requested_slot = %slot, pub_key = %pub_key, parent_hash = %parent_hash,
+            info!(requested_slot = %slot, pub_key = %pub_key, parent_hash = %parent_hash,
                 "Requested inclusion list for a slot in the past. Current slot: {}",
                 api.curr_slot_info.head_slot()
             );
-            Ok(StatusCode::NOT_FOUND.into_response())
+            let response = format!(
+                "Requested inclusion list for slot in the past. Current (slot, parent_hash, pubkey): {}, Requested (slot, parent_hash, pubkey): {}",
+                current_list.slot_coordinate, requested_slot_coordinate
+            );
+            Ok((StatusCode::NOT_FOUND, response).into_response())
         }
     }
 }
