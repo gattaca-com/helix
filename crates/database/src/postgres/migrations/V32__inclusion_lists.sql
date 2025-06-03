172aaeab0f08fa5734127c4dad26d0c16b37ef37
Jay Rabjohns <jay.r@gattaca.com>
2025-06-03 16:28:24 +0100

    fix: idempotent postgres migration for inclusion lists

--- crates/database/src/postgres/migrations/V32__inclusion_lists.sql
+++ crates/database/src/postgres/migrations/V32__inclusion_lists.sql
@@ -1,10 +1,10 @@
 ALTER TABLE validator_preferences
-ADD COLUMN "disable_inclusion_lists" boolean NOT NULL DEFAULT false;
+ADD COLUMN IF NOT EXISTS "disable_inclusion_lists" boolean NOT NULL DEFAULT false;
 
 ALTER TABLE delivered_payload_preferences
-ADD COLUMN "disable_inclusion_lists" boolean NOT NULL DEFAULT false;
+ADD COLUMN IF NOT EXISTS "disable_inclusion_lists" boolean NOT NULL DEFAULT false;
 
-CREATE TABLE "inclusion_list_txs" (
+CREATE TABLE IF NOT EXISTS "inclusion_list_txs" (
     "tx_hash" bytea NOT NULL PRIMARY KEY,
     "bytes" bytea NOT NULL,
     "nonce" bigint NOT NULL,
