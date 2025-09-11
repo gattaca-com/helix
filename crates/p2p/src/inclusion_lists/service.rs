107428403ca8952d4c672b40b13e5d868eb1e277
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-09-11 17:05:48 -0300

    feat: verify slot on each request

--- crates/p2p/src/inclusion_lists/service.rs
+++ crates/p2p/src/inclusion_lists/service.rs
@@ -16,6 +16,8 @@ pub(crate) struct MultiRelayInclusionListsService {
     cutoff_1: Duration,
     cutoff_2: Duration,
 
+    last_slot: u64,
+
     local_ils: HashMap<BlsPublicKeyBytes, (u64, InclusionList)>,
     shared_ils: HashMap<BlsPublicKeyBytes, (u64, InclusionList)>,
 }
@@ -27,15 +29,24 @@ impl MultiRelayInclusionListsService {
             cutoff_2: Duration::from_millis(p2p_api.p2p_config.cutoff_2_ms),
             local_ils: HashMap::with_capacity(p2p_api.p2p_config.peers.len()),
             shared_ils: HashMap::with_capacity(p2p_api.p2p_config.peers.len()),
+            last_slot: 0,
             p2p_api,
         }
     }
 
     pub(crate) fn handle_local_inclusion_list(&mut self, request: InclusionListRequest) {
-        trace!(slot=%request.slot, "broadcasting local inclusion list");
-        // Compute duration until cutoff 1
+        // Check request's slot is not in the past
+        if self.last_slot > request.slot {
+            warn!(slot=%request.slot, last_slot=%self.last_slot, "got local inclusion list request for a past slot, skipping");
+            let _ = request.result_tx.send(Some(request.inclusion_list));
+            return;
+        }
+
+        // Compute duration until first cutoff points
         let duration_into_slot =
             self.p2p_api.signing_context.context.duration_into_slot(request.slot.into());
+
+        // Check the slot is not in the future
         if duration_into_slot.is_none() {
             warn!("got inclusion list for a slot in the future, skipping");
             let _ = request.result_tx.send(Some(request.inclusion_list));
@@ -43,14 +54,17 @@ impl MultiRelayInclusionListsService {
         }
         let sleep_time = self.cutoff_1.saturating_sub(duration_into_slot.unwrap());
 
-        // If request was too late into the slot, skip it
+        // If request was too late into the slot, ignore it
         if sleep_time.is_zero() {
             warn!("got inclusion list too late into the slot, skipping");
             let _ = request.result_tx.send(Some(request.inclusion_list));
             return;
         }
+        self.last_slot = request.slot;
 
         let msg = InclusionListMessage::new(request.slot, request.inclusion_list.clone());
+
+        trace!(slot=%request.slot, "broadcasting local inclusion list");
         self.p2p_api.broadcast(P2PMessage::LocalInclusionList(msg));
 
         let api_requests_tx = self.p2p_api.api_requests_tx.clone();
@@ -68,6 +82,11 @@ impl MultiRelayInclusionListsService {
     }
 
     pub(crate) fn handle_shared_inclusion_list(&mut self, request: InclusionListRequest) {
+        if self.last_slot > request.slot {
+            warn!(slot=%request.slot, last_slot=%self.last_slot, "got shared inclusion list request for a past slot, skipping");
+            let _ = request.result_tx.send(Some(request.inclusion_list));
+            return;
+        }
         let InclusionListRequest { slot, inclusion_list, .. } = request;
         trace!(local_ils_count=%self.local_ils.len(), "computing shared inclusion list");
 
@@ -105,6 +124,11 @@ impl MultiRelayInclusionListsService {
     }
 
     pub(crate) fn handle_final_inclusion_list(&mut self, request: InclusionListRequest) {
+        if self.last_slot > request.slot {
+            warn!(slot=%request.slot, last_slot=%self.last_slot, "got final inclusion list request for a past slot, skipping");
+            let _ = request.result_tx.send(Some(request.inclusion_list));
+            return;
+        }
         let InclusionListRequest { slot, inclusion_list, result_tx } = request;
         trace!(shared_ils_count=%self.shared_ils.len(), "computing final inclusion list");
 
@@ -124,6 +148,10 @@ impl MultiRelayInclusionListsService {
         sender: BlsPublicKeyBytes,
         il_msg: InclusionListMessage,
     ) {
+        if self.last_slot > il_msg.slot {
+            warn!(slot=%il_msg.slot, last_slot=%self.last_slot, "got local inclusion list from peer for a past slot, skipping");
+            return;
+        }
         trace!(peer=%sender, "got local IL from peer");
         self.local_ils.insert(sender, (il_msg.slot, il_msg.inclusion_list));
     }
@@ -133,6 +161,10 @@ impl MultiRelayInclusionListsService {
         sender: BlsPublicKeyBytes,
         il_msg: InclusionListMessage,
     ) {
+        if self.last_slot > il_msg.slot {
+            warn!(slot=%il_msg.slot, last_slot=%self.last_slot, "got shared inclusion list from peer for a past slot, skipping");
+            return;
+        }
         trace!(peer=%sender, "got shared IL from peer");
         self.shared_ils.insert(sender, (il_msg.slot, il_msg.inclusion_list));
     }
