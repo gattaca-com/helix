8f187f365999e32836c4338b250be32e202985db
owen <owen@gattaca.com>
2025-09-30 09:49:24 +0100

    moving alert to before we await the handle as it can timeout

--- crates/api/src/proposer/get_payload.rs
+++ crates/api/src/proposer/get_payload.rs
@@ -294,6 +294,10 @@ impl<A: Api> ProposerApi<A> {
             (trace_clone, failed_publishing)
         });
 
+        if let Some(merged_block) = self.auctioneer.get_merged_block(&block_hash) {
+            self.alert_manager.send(&merged_block.to_alert_message());
+        }
+
         if !is_trusted_proposer {
             let Ok((new_trace, failed_publishing)) = handle.await else {
                 return Err(ProposerApiError::InternalServerError);
@@ -324,10 +328,6 @@ impl<A: Api> ProposerApi<A> {
 
         let get_payload_response =
             GetPayloadResponse { version: fork, metadata: Default::default(), data: payload };
-
-        if let Some(merged_block) = self.auctioneer.get_merged_block(&block_hash) {
-            self.alert_manager.send(&merged_block.to_alert_message());
-        }
 
         // Return response
         info!(?trace, timestamp = utcnow_ns(), "delivering payload");
