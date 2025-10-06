9149b803795e71aba391d946a5b51f465af7c433
eltitanb <lorenzo@gattaca.com>
2025-10-06 19:53:15 +0100

    increase reg queue size

--- crates/api/src/auctioneer/mod.rs
+++ crates/api/src/auctioneer/mod.rs
@@ -54,7 +54,7 @@ pub fn spawn_workers<A: Api>(
     slot_data_rx: crossbeam_channel::Receiver<HkSlotData>,
 ) -> (AuctioneerHandle, RegWorkerHandle) {
     let (sub_worker_tx, sub_worker_rx) = crossbeam_channel::bounded(10_000);
-    let (reg_worker_tx, reg_worker_rx) = crossbeam_channel::bounded(10_000);
+    let (reg_worker_tx, reg_worker_rx) = crossbeam_channel::bounded(100_000);
     let (event_tx, event_rx) = crossbeam_channel::bounded(10_000);
 
     if config.is_registration_instance {
