e9544f5422963016aa051c51480424d1a4716b94
eltitanb <lorenzo@gattaca.com>
2025-10-18 16:27:02 +0100

    fix proto build.rs

--- crates/relay/build.rs
+++ crates/relay/build.rs
@@ -1,8 +1,14 @@
+use std::path::PathBuf;
+
 fn main() {
+    let manifest_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
+    let proto_file = manifest_dir.join("src/api/gossiper/proto/gossipclient.proto");
+    let include_dir = manifest_dir.join("src/api/gossiper/proto");
+
     tonic_build::configure()
         .build_client(true)
         .build_server(true)
-        .compile(&["src/api/gossiper/proto/gossipclient.proto"], &["src/gossiper/proto"])
+        .compile(&[proto_file], &[include_dir])
         .unwrap();
 
     println!("cargo:rerun-if-changed=src/database/postgres/migrations");
