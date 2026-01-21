ecbccb6c378d17bd88e73da83e5389194c491073
Nina <nina@gattaca.com>
2026-01-21 19:03:03 +0000

    v2

--- crates/common/src/metrics.rs
+++ crates/common/src/metrics.rs
@@ -607,6 +607,15 @@ lazy_static! {
         &RELAY_METRICS_REGISTRY
     )
     .unwrap();
+
+    pub static ref BID_ADJUSTMENT_APPLICATION_LATENCY_V2: HistogramVec = register_histogram_vec_with_registry!(
+        "bid_adjustment_application_latency_v2_us",
+        "Latency of applying bid adjustments in us",
+        &["type"],
+        vec![1., 5., 10., 15., 25., 50., 100., 250., 500., 1_000., 5_000., 10_000., 25_000., 50_000., 100_000., 500_000., 1_000_000., 5_000_000., 10_000_000., 50_000_000., 100_000_000.,],
+        &RELAY_METRICS_REGISTRY
+    )
+    .unwrap();
 }
 
 pub struct ApiMetrics {
